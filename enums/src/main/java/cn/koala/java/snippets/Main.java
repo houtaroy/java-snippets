318125ef7e289f7d56d2f0a67e01f7162b832a4c
houtaroy <houtaroy@gmail.com>
2024-03-04 15:45:51 +0800

    feat(仓库): 使用gradle初始化模块

---
+++ enums/src/main/java/cn/koala/java/snippets/Main.java
@@ -0,0 +1,12 @@
+package cn.koala.java.snippets;
+
+/**
+ * Main
+ *
+ * @author Houtaroy
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
